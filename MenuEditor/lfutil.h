1e57fa4c844e980937635dccad0e085b9530637c
Claybird <claybird.without.wing@gmail.com>
2024-03-06 22:37:18 +0900

    removed MenuEditor

--- MenuEditor/lfutil.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-//複数階層のディレクトリを一気に作成する
-BOOL UtilMakeSureDirectoryPathExists(LPCTSTR lpszPath);
-//INIに指定されたセクションがあるならtrueを返す
-bool UtilCheckINISectionExists(LPCTSTR lpAppName,LPCTSTR lpFileName);
--- MenuEditor/stdafx.cpp
+++
@@ -1,3 +0,0 @@
-#include "stdafx.h"
-
-//a dummy file for pre-compiled header
